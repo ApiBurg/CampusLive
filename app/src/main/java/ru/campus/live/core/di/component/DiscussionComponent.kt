597024aea9e5d29ccade99c88181b87c7bcd30d5
alex <apiburg@bk.ru>
2022-04-08 23:00:18 +0300

    Оптимизировал discussionComponent

--- app/src/main/java/ru/campus/live/core/di/component/DiscussionComponent.kt
+++ app/src/main/java/ru/campus/live/core/di/component/DiscussionComponent.kt
@@ -3,13 +3,9 @@ package ru.campus.live.core.di.component
 import dagger.Component
 import ru.campus.live.core.di.deps.AppDeps
 import ru.campus.live.core.di.module.DiscussionModule
-import ru.campus.live.core.di.module.viewmodel.DiscussionVModule
 import ru.campus.live.core.di.module.viewmodel.base.ViewModelFactory
 
-@Component(
-    modules = [DiscussionModule::class, DiscussionVModule::class],
-    dependencies = [AppDeps::class]
-)
+@Component(modules = [DiscussionModule::class], dependencies = [AppDeps::class])
 interface DiscussionComponent {
 
     fun viewModelsFactory(): ViewModelFactory
--- app/src/main/java/ru/campus/live/core/di/module/DiscussionModule.kt
+++ app/src/main/java/ru/campus/live/core/di/module/DiscussionModule.kt
@@ -1,45 +1,23 @@
 package ru.campus.live.core.di.module
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
-import ru.campus.live.core.data.datasource.ErrorDataSource
-import ru.campus.live.core.data.datasource.UserDataSource
-import ru.campus.live.core.data.APIService
 import ru.campus.live.core.data.repository.IUploadMediaRepository
 import ru.campus.live.core.data.repository.UploadMediaRepository
-import ru.campus.live.core.domain.PreparationMediaUseCase
+import ru.campus.live.core.di.module.viewmodel.DiscussionVModule
 import ru.campus.live.discussion.data.repository.DiscussionRepository
 import ru.campus.live.discussion.data.repository.IDiscussionRepository
 
+@Module(includes = [DiscussionBindModule::class, DiscussionVModule::class])
+class DiscussionModule
+
 @Module
-class DiscussionModule {
+interface DiscussionBindModule {
 
-    @Provides
-    fun provideDiscussionRepository(
-        apiService: APIService,
-        errorDataSource: ErrorDataSource,
-        userDataSource: UserDataSource
-    ): DiscussionRepository {
-        return DiscussionRepository(apiService, errorDataSource, userDataSource)
-    }
+    @Binds
+    fun bindDiscussionRepository(discussionRepository: DiscussionRepository): IDiscussionRepository
 
-    @Provides
-    fun provideIDiscussionRepository(discussionRepository: DiscussionRepository): IDiscussionRepository {
-        return discussionRepository
-    }
-
-    @Provides
-    fun provideUploadMediaRepository(
-        apiService: APIService,
-        errorDataSource: ErrorDataSource,
-        preparationMediaUseCase: PreparationMediaUseCase
-    ): UploadMediaRepository {
-        return UploadMediaRepository(apiService, errorDataSource, preparationMediaUseCase)
-    }
-
-    @Provides
-    fun provideIUploadMediaRepository(uploadMediaRepository: UploadMediaRepository): IUploadMediaRepository {
-        return uploadMediaRepository
-    }
+    @Binds
+    fun bindUploadMediaRepository(uploadMediaRepository: UploadMediaRepository): IUploadMediaRepository
 
 }
