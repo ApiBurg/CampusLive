b11be6331e6488a833f76d160f92fe9b0ff9a973
alex <soloviev_alexey@bk.ru>
2022-04-14 21:10:29 +0300

    Изменил название параметра

--- app/src/main/java/ru/campus/live/location/adapter/holder/LocationViewHolder.kt
+++ app/src/main/java/ru/campus/live/location/adapter/holder/LocationViewHolder.kt
@@ -1,20 +1,20 @@
 package ru.campus.live.location.adapter.holder
 
 import androidx.recyclerview.widget.RecyclerView
-import ru.campus.live.location.data.model.LocationModel
-import ru.campus.live.databinding.ItemLocationBinding
 import ru.campus.live.core.presentation.ui.MyOnClick
+import ru.campus.live.databinding.ItemLocationBinding
+import ru.campus.live.location.data.model.LocationModel
 
 class LocationViewHolder(
     private val itemBinding: ItemLocationBinding,
-    private val myOnClick: MyOnClick<LocationModel>
+    private val myOnClick: MyOnClick<LocationModel>,
 ) : RecyclerView.ViewHolder(itemBinding.root) {
 
-    fun bind(model: LocationModel) {
-        itemBinding.name.text = model.name
-        itemBinding.address.text = model.address
+    fun bind(item: LocationModel) {
+        itemBinding.name.text = item.name
+        itemBinding.address.text = item.address
         itemBinding.container.setOnClickListener {
-            myOnClick.item(itemBinding.container, model)
+            myOnClick.item(itemBinding.container, item)
         }
     }
 
